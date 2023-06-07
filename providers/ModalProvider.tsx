e02ce7e7edd1131e81e036d2ca665ad7c95385b6
awais512 <raza.awais25@gmail.com>
2023-06-07 14:23:13 +0100

    Generic Modal Done

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+import Modal from "@/components/Modal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <>
+      <Modal
+        title="Test Modal"
+        description="Test Modal Description"
+        isOpen
+        onChange={() => {}}
+      >
+        Test Children
+      </Modal>
+    </>
+  );
+};
+
+export default ModalProvider;
