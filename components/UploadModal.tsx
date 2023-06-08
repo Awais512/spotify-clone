ecaf72cf648a38ae9a11e72a4e1db757654c443e
awais512 <raza.awais25@gmail.com>
2023-06-09 00:36:45 +0100

    UploadModal Ui

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -4,6 +4,7 @@ import Modal from "./Modal";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import Input from "./Input";
+import Button from "./Button";
 
 const UploadModal = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -35,13 +36,42 @@ const UploadModal = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form className="flex flex-col gap-y-4" onSubmit={handleSubmit(onSubmit)}>
         <Input
           id="title"
           disabled={isLoading}
           {...register("title", { required: true })}
           placeholder="Song title"
         />
+        <Input
+          id="author"
+          disabled={isLoading}
+          {...register("author", { required: true })}
+          placeholder="Song author"
+        />
+        <div>
+          <div className="pb-1">Select a song file</div>
+          <Input
+            id="song"
+            type="file"
+            disabled={isLoading}
+            {...register("song", { required: true })}
+            accept=".mp3"
+          />
+        </div>
+        <div>
+          <div className="pb-1">Select an image</div>
+          <Input
+            id="image"
+            type="file"
+            disabled={isLoading}
+            {...register("image", { required: true })}
+            accept="image/*"
+          />
+        </div>
+        <Button disabled={isLoading} type="submit">
+          Create
+        </Button>
       </form>
     </Modal>
   );
