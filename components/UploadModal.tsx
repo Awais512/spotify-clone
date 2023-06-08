fbd4be6bb16a793f72653e347b6d23f176b9fea9
awais512 <raza.awais25@gmail.com>
2023-06-09 00:19:35 +0100

    Input Component

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,16 +1,32 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
 import useUploadModal from "@/hooks/useUploadModal";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import Input from "./Input";
 
 const UploadModal = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const uploadModal = useUploadModal();
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
 
   const onChange = (open: boolean) => {
     if (!open) {
+      reset();
       uploadModal.onClose();
     }
   };
+
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {};
 
   return (
     <Modal
@@ -19,7 +35,14 @@ const UploadModal = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      Form
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song title"
+        />
+      </form>
     </Modal>
   );
 };
